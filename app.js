7a38cf354467aa2c031094a55862e2d1704aba2b
Jimmy Beblein Poulsen <jimm6288@edu.campusvejle.dk>
2020-03-17 12:04:56 +0100

    added event to app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var stuff = require('./stuff')
+// var stuff = require('./stuff')
 
-console.log(stuff.counter(['num1', 'num2','num3']))
-console.log(stuff.adder(5,6))
-console.log(stuff.adder(stuff.pi, 5))
+// console.log(stuff.counter(['num1', 'num2','num3']))
+// console.log(stuff.adder(5,6))
+// console.log(stuff.adder(stuff.pi, 5))
+
+var events = require('events');
+
+var myEmitter = new events.EventEmitter();
+
+myEmitter.on('someEvent', function(msg){
+    console.log(msg)
+})
+
+myEmitter.emit('someEvent', 'the event was emitted')
